b06dea9bd61626572799d210313eade563436f60
Joumana <joumanasalahedin@gmail.com>
2024-01-08 23:54:32 +0100

    task 4 stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """status route"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', method=["GET"], strict_slashes=False)
+def stats():
+    """number of each object by type"""
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    stats = jsonify(data)
+    stats.status_code = 200
+
+    return stats
