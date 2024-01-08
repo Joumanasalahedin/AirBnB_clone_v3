3064425f3228ce9759c46c8f85d698214227ef66
thumeka <thumekatembani21@gmail.com>
2024-01-08 03:34:18 -0800

    index2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,29 +3,9 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
-    '''
-        return JSON of OK status
-    '''
-    json_dict = {"status": "OK")
-    return jsonify(json_dict)
-
-
-@app_views.route("/stats", strict_slashes=False)
-def stats:
-    '''
-        return counts of all classes in storage
-    '''
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    """status route"""
+    return jsonify(status="OK")
