2d6f236a7b41175d2e31d4dcdc85a1173d381e5b
thumeka <thumekatembani21@gmail.com>
2024-01-08 16:46:27 -0800

    user

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,6 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        encripted = md5(str.encode(kwargs["password"])).hexdigest()
+        kwargs["password"] = encripted
         super().__init__(*args, **kwargs)
